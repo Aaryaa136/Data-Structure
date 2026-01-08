7a1943f0915f61ca3724197f71c0b87382238b9d
Aaryaa136 <patilaaryaa85@gmail.com>
2026-01-09 01:25:19 +0530

    Add files via upload

---
+++ DS Programs/program413.c
@@ -0,0 +1,48 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+struct node
+{
+    int data;
+    struct node* next;
+    struct node* prev;
+};
+
+typedef struct node NODE;
+typedef struct node* PNODE;
+typedef struct node** PPNODE;
+
+void InsertFirst(PPNODE first , PPNODE last , int no)
+{}
+
+void InsertLast(PPNODE first , PPNODE last , int no)
+{}
+
+void DeleteFirst(PPNODE first , PPNODE last)
+{}
+
+void DeleteLast(PPNODE first , PPNODE last)
+{}
+
+void Display(PNODE first , PPNODE last)
+{}
+
+int Count(PPNODE first , PPNODE last)
+{
+    return 0;
+}
+
+void InsertAtPos(PPNODE first , PPNODE last , int no , int pos)
+{}
+
+void DeleteAtPos(PPNODE first , PPNODE last , int pos)
+{}
+
+
+int main()
+{
+    PNODE head = NULL;
+    PNODE tail = NULL;
+
+    return 0;
+}
